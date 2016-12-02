4ee419ebd090918244e96657923e7f977e309c74
Travis Reeder <treeder@gmail.com>
2016-12-01 16:04:02 -0800

    operator: 0.0.16 release

--- release.sh
+++ release.sh
@@ -6,14 +6,9 @@ service="operator"
 version_file="main.go"
 tag="latest"
 
-if [ -z $(grep -Eo "[0-9]+\.[0-9]+\.[0-9]+" $version_file) ]; then
-  echo "did not find semantic version in $version_file"
-  exit 1
-fi
-
-perl -i -pe 's/\d+\.\d+\.\K(\d+)/$1+1/e' $version_file
-version=$(grep -Eo "[0-9]+\.[0-9]+\.[0-9]+" $version_file)
-echo "Version: $version"
+docker run --rm -v "$PWD":/app treeder/bump patch
+version=`cat VERSION`
+echo "version: $version"
 
 ./build.sh
 
