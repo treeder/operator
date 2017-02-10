1d14514e908792fcb338b5c01c0e1022af46d19d
Travis Reeder <treeder@gmail.com>
2017-02-10 09:44:07 -0800

    Updated command help info

--- cmd/instances.go
+++ cmd/instances.go
@@ -24,13 +24,8 @@ import (
 // instancesCmd represents the instances command
 var instancesCmd = &cobra.Command{
 	Use:   "instances",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "List all instances of an app",
+	Long:  `List all instance sof an app, be sure to pass in --name flag.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		ctx := context.Background()
 
--- cmd/kill.go
+++ cmd/kill.go
@@ -24,13 +24,12 @@ import (
 // killCmd represents the kill command
 var killCmd = &cobra.Command{
 	Use:   "kill",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Kills servers",
+	Long: `Kills a server by passing in a an instance ID. 
+	
+	eg: docker run treeder/operator kill INSTANCE_ID
+	
+	Get instance ids for an app with treeder/operator instances`,
 	Run: func(cmd *cobra.Command, args []string) {
 		ctx := context.Background()
 		logrus.Infoln("args", args)
